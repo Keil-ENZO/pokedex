9ab02b61a43c50765e0ccc88b96b4d04bf0c4e0a
Keil-ENZO <enzo.keil06@icloud.com>
2024-10-03 18:29:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+// https://pokeapi.co/api/v2/pokemon
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div>
+            <p className={"bg-red-500"}>test</p>
+          <Button>Button</Button>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
